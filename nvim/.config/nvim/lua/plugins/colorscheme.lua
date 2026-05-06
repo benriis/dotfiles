193042d6bbae07fc29fcd0ab44e2f82dd607fd45
benjamin <benjaymin.p@gmail.com>
2026-05-06 11:04:34 +0200

    .

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,5 +2,7 @@ vim.pack.add({
 	{ src = "https://github.com/zenbones-theme/zenbones.nvim" },
 	{ src = "https://github.com/rktjmp/lush.nvim" },
 	{ src = "https://github.com/ellisonleao/gruvbox.nvim" },
+	{ src = "https://github.com/miikanissi/modus-themes.nvim" },
 })
-vim.cmd("colorscheme neobones")
+
+vim.cmd("colorscheme zenbones")
